4de65ae3a000543f7e668f719b649b073952c8bc
thenileshmishra <thenileshmishra@gmail.com>
2025-12-23 15:55:33 +0530

    ingest.py and custom exception

---
+++ src/exception.py
@@ -0,0 +1,31 @@
+import sys
+import logging
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+
+    if exc_tb in None:
+        file_name = "<unknown>"
+        line_no = 0
+    else:
+        tb = exc_tb
+        while tb.tb_next:
+            tb = tb.tb_next
+        file_name = tb.tb_frame.f_code.co_filename
+        line_no = tb.tb_lineno
+
+    error_message = "Error occurred in python script [{0}] at line [{1}] with message: [{2}]".format(
+        file_name,
+        line_no,
+        str(error)
+    )
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail = error_details)
+
+    def __str__(self):
+        return self.error_message
---
+++ src/ingest.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from pathlib import Path
+from .config import RAW_DATA_DIR, PROCESSED_DATA_DIR, TRAIN_FILE, TEST_FILE
+from src.exception import CustomException
+import sys
+
+
+def load_raw_data():
+    try:
+        train_path = RAW_DATA_DIR / "train.csv"
+        test_path = RAW_DATA_DIR / "test.csv"
+
+        if not train_path.exists():
+            raise CustomException(f"Missing file: {train_path}")
+
+        if not test_path.exists():
+            raise CustomException(f"Missing file: {test_path}")
+
+        train_df = pd.read_csv(train_path)
+        test_df = pd.read_csv(test_path)
+
+        return train_df, test_df
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def basic_quality_check(train_df, test_df):
+    try:
+        if train_df.empty:
+            raise CustomException("Training dataframe is empty")
+
+        if test_df.empty:
+            raise CustomException("Test dataframe is empty")
+        
+        if "diagnosed_diabetes" not in train_df.columns:
+            raise CustomException("Target column 'diagnosed_diabetes' missing in train dataset")
+        
+        if train_df.duplicated().sum() > 0:
+            train_df.drop_duplicates(input=True)
+
+        return train_df, test_df
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def save_processed(train_df, test_df):
+    PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+    train_df.to_csv(TRAIN_FILE, index=False)
+    test_df.to_csv(TEST_FILE, index=False)
+
+    print(f"Processed train saved -> {TRAIN_FILE}")
+    print(f"Processed test saved -> {TEST_FILE}")
+
+def run_ingestion():
+    print("Loading raw data...")
+    train_df, test_df = load_raw_data()
+
+    print("Running quality checks...")
+    train_df, test_df = basic_quality_check(train_df, test_df)
+
+    print("Saving processed data...")
+    save_processed(train_df, test_df)
+
+    print("Ingestion completed succesfully.")
+
+if __name__ == "__main__":
+    run_ingestion()
+    
